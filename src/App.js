8072f4a4ce9bb6a4087cf7039923afc296ebc7c4
Caroline Clark <carolineclark@Carolines-MacBook-Pro-3.local>
2025-04-14 18:49:24 -0400

    Initial Push

--- src/App.js
+++ src/App.js
@@ -1,42 +1,35 @@
-// src/App.js
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import styled from 'styled-components';
-
-const AppWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-`;
-
-const MainContent = styled.main`
-  flex: 1;
-`;
 
 function App() {
   return (
-    <AppWrapper>
-      <Navbar />
-      <MainContent>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </MainContent>
-      <Footer />
-    </AppWrapper>
+    <Router>
+      <div className="app-wrapper">
+        <Navbar />
+
+        <main style={{ flexGrow: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
 
 
 
